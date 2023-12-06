7636add785efa856c5b9dae5a2a84d754af5512c
Lahiru Vimukthi <lahiruvimukthi82@gmail.com>
2023-12-06 20:32:03 +0530

    add form1 design file

---
+++ Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Enterprise_Systems_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button13 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SOut = new System.Windows.Forms.TextBox();
+            this.SIn = new System.Windows.Forms.TextBox();
+            this.IStock = new System.Windows.Forms.TextBox();
+            this.RPrice = new System.Windows.Forms.TextBox();
+            this.CPrice = new System.Windows.Forms.TextBox();
+            this.IName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Cost_Price = new System.Windows.Forms.Label();
+            this.item_name = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button13.Location = new System.Drawing.Point(37, 407);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(162, 39);
+            this.button13.TabIndex = 56;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button11.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button11.Location = new System.Drawing.Point(37, 355);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(162, 39);
+            this.button11.TabIndex = 55;
+            this.button11.Text = "Registration";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button10.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button10.Location = new System.Drawing.Point(37, 294);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(162, 39);
+            this.button10.TabIndex = 54;
+            this.button10.Text = "Login";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button9.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button9.Location = new System.Drawing.Point(37, 469);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(162, 39);
+            this.button9.TabIndex = 53;
+            this.button9.Text = "LogOut";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(32, 56);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(134, 27);
+            this.label12.TabIndex = 52;
+            this.label12.Text = "Dashboard";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button3.Location = new System.Drawing.Point(37, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 39);
+            this.button3.TabIndex = 51;
+            this.button3.Text = "Manage Items";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button2.Location = new System.Drawing.Point(37, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 39);
+            this.button2.TabIndex = 50;
+            this.button2.Text = "Manage Category";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(37, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 39);
+            this.button1.TabIndex = 49;
+            this.button1.Text = "Seller Details";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.SOut);
+            this.panel1.Controls.Add(this.SIn);
+            this.panel1.Controls.Add(this.IStock);
+            this.panel1.Controls.Add(this.RPrice);
+            this.panel1.Controls.Add(this.CPrice);
+            this.panel1.Controls.Add(this.IName);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Cost_Price);
+            this.panel1.Controls.Add(this.item_name);
+            this.panel1.Location = new System.Drawing.Point(230, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(789, 489);
+            this.panel1.TabIndex = 48;
+            // 
+            // SOut
+            // 
+            this.SOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SOut.Location = new System.Drawing.Point(190, 300);
+            this.SOut.Name = "SOut";
+            this.SOut.Size = new System.Drawing.Size(176, 28);
+            this.SOut.TabIndex = 42;
+            // 
+            // SIn
+            // 
+            this.SIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SIn.Location = new System.Drawing.Point(190, 258);
+            this.SIn.Name = "SIn";
+            this.SIn.Size = new System.Drawing.Size(176, 28);
+            this.SIn.TabIndex = 41;
+            // 
+            // IStock
+            // 
+            this.IStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IStock.Location = new System.Drawing.Point(190, 215);
+            this.IStock.Name = "IStock";
+            this.IStock.Size = new System.Drawing.Size(176, 28);
+            this.IStock.TabIndex = 40;
+            // 
+            // RPrice
+            // 
+            this.RPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RPrice.Location = new System.Drawing.Point(190, 171);
+            this.RPrice.Name = "RPrice";
+            this.RPrice.Size = new System.Drawing.Size(176, 28);
+            this.RPrice.TabIndex = 39;
+            // 
+            // CPrice
+            // 
+            this.CPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CPrice.Location = new System.Drawing.Point(190, 125);
+            this.CPrice.Name = "CPrice";
+            this.CPrice.Size = new System.Drawing.Size(176, 28);
+            this.CPrice.TabIndex = 38;
+            // 
+            // IName
+            // 
+            this.IName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IName.Location = new System.Drawing.Point(190, 80);
+            this.IName.Name = "IName";
+            this.IName.Size = new System.Drawing.Size(176, 28);
+            this.IName.TabIndex = 37;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label6.Font = new System.Drawing.Font("Arial", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label6.Location = new System.Drawing.Point(238, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(302, 35);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "ITEM INFORMATION";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label5.Location = new System.Drawing.Point(38, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Stock Out";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(39, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Stock In";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(39, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Initial Stock";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(39, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Retail Price";
+            // 
+            // Cost_Price
+            // 
+            this.Cost_Price.AutoSize = true;
+            this.Cost_Price.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cost_Price.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Cost_Price.Location = new System.Drawing.Point(39, 125);
+            this.Cost_Price.Name = "Cost_Price";
+            this.Cost_Price.Size = new System.Drawing.Size(108, 24);
+            this.Cost_Price.TabIndex = 2;
+            this.Cost_Price.Text = "Cost Price";
+            // 
+            // item_name
+            // 
+            this.item_name.AutoSize = true;
+            this.item_name.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item_name.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.item_name.Location = new System.Drawing.Point(38, 80);
+            this.item_name.Name = "item_name";
+            this.item_name.Size = new System.Drawing.Size(109, 24);
+            this.item_name.TabIndex = 1;
+            this.item_name.Text = "Item Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 599);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox SOut;
+        private System.Windows.Forms.TextBox SIn;
+        private System.Windows.Forms.TextBox IStock;
+        private System.Windows.Forms.TextBox RPrice;
+        private System.Windows.Forms.TextBox CPrice;
+        private System.Windows.Forms.TextBox IName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Cost_Price;
+        private System.Windows.Forms.Label item_name;
+    }
+}
